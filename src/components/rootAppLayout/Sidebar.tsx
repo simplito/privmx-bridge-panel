d8cfad1fb34f676b4b1bf423dad570b6a9f4f3cc
Witold Pazderski <witold.pazderski@gmail.com>
2025-03-25 06:31:10 +0100

    Rework the main sidebar

--- src/components/rootAppLayout/Sidebar.tsx
+++ src/components/rootAppLayout/Sidebar.tsx
@@ -1,16 +1,27 @@
-import { AppShell, NavLink, ScrollArea, Stack } from "@mantine/core";
+import { AppShell, Box, NavLink, ScrollArea, Stack } from "@mantine/core";
+import { useCallback, useState } from "react";
 import { useTranslations } from "use-intl";
 import { appRoutes } from "@/app/appRoutes";
 import { useAuthData } from "@/hooks/useAuthData";
 import { Link } from "@/i18n/routing";
 import { Icon } from "../atoms/Icon";
-import { Button } from "../button/Button";
 import { colors } from "./mantineTheme";
+import { SidebarContexts } from "./SidebarContexts";
+import { SidebarSolutions } from "./SidebarSolutions";
 
 export function Sidebar() {
     const t = useTranslations();
     const { authData } = useAuthData();
     const isSignedIn = authData.privMxBridgeApiAuthData !== null;
+    const [isContextsListOpen, setIsContextsListOpen] = useState(false);
+    const [isSolutionsListOpen, setIsSolutionsListOpen] = useState(false);
+
+    const handleToggleContextsListClick = useCallback(() => {
+        setIsContextsListOpen((prev) => !prev);
+    }, []);
+    const handleToggleSolutionsListClick = useCallback(() => {
+        setIsSolutionsListOpen((prev) => !prev);
+    }, []);
 
     return (
         <AppShell.Navbar withBorder={false} bg={colors["document/backgrounds/body"]}>
@@ -20,9 +31,32 @@ export function Sidebar() {
                     <NavLink
                         label={t("mainNav.solutions")}
                         leftSection={<Icon name="solutions" size={"md"} />}
+                        rightSection={
+                            <div onClick={handleToggleSolutionsListClick}>
+                                <Icon name={isSolutionsListOpen ? "chevronDown" : "chevronRight"} size={"sm"} />
+                            </div>
+                        }
                         href={appRoutes.solutions.list()}
                         component={Link}
                     />
+                ) : null}
+                {isSignedIn && isSolutionsListOpen ? <SidebarSolutions /> : null}
+                {isSignedIn ? (
+                    <NavLink
+                        label={t("mainNav.contexts")}
+                        leftSection={<Icon name="contexts" size={"md"} />}
+                        rightSection={
+                            <div onClick={handleToggleContextsListClick}>
+                                <Icon name={isContextsListOpen ? "chevronDown" : "chevronRight"} size={"sm"} />
+                            </div>
+                        }
+                        href={appRoutes.contexts.list()}
+                        component={Link}
+                    />
+                ) : null}
+                {isSignedIn && isContextsListOpen ? <SidebarContexts /> : null}
+                {isSignedIn ? (
+                    <NavLink label={t("mainNav.users")} leftSection={<Icon name="users" size={"md"} />} href={appRoutes.users.list()} component={Link} />
                 ) : null}
                 {isSignedIn ? (
                     <NavLink
@@ -32,13 +66,41 @@ export function Sidebar() {
                         component={Link}
                     />
                 ) : null}
+                <Box mt="md" style={{ borderBottom: `1px solid ${colors["document/backgrounds/grid"]}` }} />
             </AppShell.Section>
             <AppShell.Section>
-                <Stack gap="md" align="stretch" p="md" pb="xl">
+                <Stack
+                    gap="md"
+                    align="stretch"
+                    pb="sm"
+                    pt="sm"
+                    mb="xl"
+                    style={{ borderBottom: `1px solid ${colors["document/backgrounds/grid"]}`, borderTop: `1px solid ${colors["document/backgrounds/grid"]}` }}
+                >
                     {isSignedIn ? (
-                        <Button type="link" preset="signOut" href={appRoutes.auth.signOut()} />
+                        <NavLink
+                            label={t("forms.buttons.signOut")}
+                            rightSection={
+                                <span style={{ color: colors["document/typography/text"] }}>
+                                    <Icon name="signOut" size={"md"} />
+                                </span>
+                            }
+                            href={appRoutes.auth.signOut()}
+                            pl="md"
+                            component={Link}
+                        />
                     ) : (
-                        <Button type="link" preset="signIn" href={appRoutes.auth.signIn()} />
+                        <NavLink
+                            label={t("forms.buttons.signIn")}
+                            rightSection={
+                                <span style={{ color: colors["document/typography/text"] }}>
+                                    <Icon name="signIn" size={"md"} />
+                                </span>
+                            }
+                            href={appRoutes.auth.signIn()}
+                            pl="md"
+                            component={Link}
+                        />
                     )}
                 </Stack>
             </AppShell.Section>
