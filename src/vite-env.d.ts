a0c7ffc4de7eddcd34d6321073eb5004f8158a25
Witold Pazderski <witold.pazderski@gmail.com>
2025-03-25 06:25:38 +0100

    Add svg-related types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module "*.svg" {
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    import React = require("react");
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    // eslint-disable-next-line import/no-default-export
+    export default src;
+}
