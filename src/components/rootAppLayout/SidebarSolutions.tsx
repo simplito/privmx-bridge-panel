f758e8986e1301cbe71c2fd8e487dd7dc0416cfe
Witold Pazderski <witold.pazderski@gmail.com>
2025-03-25 06:28:38 +0100

    Add sidebar Solutions module

---
+++ src/components/rootAppLayout/SidebarSolutions.tsx
@@ -0,0 +1,87 @@
+import { NavLink } from "@mantine/core";
+import type * as ServerApiTypes from "privmx-server-api";
+import { useCallback, useState } from "react";
+import { useTranslations } from "use-intl";
+import { appRoutes } from "@/app/appRoutes";
+import { useOpenCreateSolutionModal } from "@/features/solutions/create/openCreateSolutionModal";
+import { useAuthData } from "@/hooks/useAuthData";
+import { useDataLoader } from "@/hooks/useDataLoader";
+import { usePrivMxBridgeApiEventListener } from "@/hooks/usePrivMxBridgeApiEventListener";
+import { useSolutionApi } from "@/hooks/useSolutionApi";
+import { Link } from "@/i18n/routing";
+import type { SolutionApi } from "@/privMxBridgeApi/SolutionApi";
+import { Icon } from "../atoms/Icon";
+import { LoadingOrError } from "../atoms/LoadingOrError";
+
+async function loadSolutions(solutionApi: SolutionApi): Promise<ServerApiTypes.api.solution.Solution[]> {
+    const res = await solutionApi.listSolutions();
+    return res.list.sort((a, b) => b.created - a.created);
+}
+
+const defaultSolutionsCount = 3;
+
+export function SidebarSolutions() {
+    const t = useTranslations();
+    const { openCreateSolutionModal } = useOpenCreateSolutionModal();
+    const { authData } = useAuthData();
+    const isSignedIn = authData.privMxBridgeApiAuthData !== null;
+    const solutionApi = useSolutionApi();
+    const [shouldShowAllSolutions, setShouldShowAllSolutions] = useState(false);
+    const [solutions, setSolutions] = useState<ServerApiTypes.api.solution.Solution[]>([]);
+    const solutionsLoader = useCallback(async () => {
+        if (!isSignedIn) {
+            return [];
+        }
+        return await loadSolutions(solutionApi);
+    }, [solutionApi, isSignedIn]);
+    const { isLoading: isLoadingSolutions, error: solutionsLoadingError, reload: reloadSolutions } = useDataLoader(solutionsLoader, setSolutions);
+    usePrivMxBridgeApiEventListener("solutionsChanged", reloadSolutions);
+    const shouldShowMoreSolutionsButton = solutions.length > defaultSolutionsCount && !shouldShowAllSolutions;
+    const handleToggleSolutionsListClick = useCallback(() => {
+        setShouldShowAllSolutions((prev) => !prev);
+    }, []);
+    const handleCreateSolutionClick = useCallback(() => {
+        void openCreateSolutionModal();
+    }, [openCreateSolutionModal]);
+
+    if (!isSignedIn) {
+        return null;
+    }
+    if (isLoadingSolutions || solutionsLoadingError !== null) {
+        return <LoadingOrError error={solutionsLoadingError} isLoading={isLoadingSolutions} loaderSize="sm" />;
+    }
+
+    return (
+        <>
+            {solutions.map((solution, idx) => {
+                if (!shouldShowAllSolutions && idx >= defaultSolutionsCount) {
+                    return null;
+                }
+                return (
+                    <NavLink
+                        key={solution.id}
+                        label={solution.name}
+                        leftSection={<Icon name="solution" size={"md"} />}
+                        href={appRoutes.solutions.$solution(solution.id).profile()}
+                        component={Link}
+                        style={{ paddingLeft: "30px" }}
+                    />
+                );
+            })}
+            {shouldShowMoreSolutionsButton ? (
+                <NavLink
+                    label={t("mainNav.navActions.expand")}
+                    leftSection={<Icon name="more" size={"md"} />}
+                    onClick={handleToggleSolutionsListClick}
+                    style={{ paddingLeft: "30px" }}
+                />
+            ) : null}
+            <NavLink
+                label={t("mainNav.navActions.createNewSolution")}
+                leftSection={<Icon name="add" size={"md"} />}
+                onClick={handleCreateSolutionClick}
+                style={{ paddingLeft: "30px" }}
+            />
+        </>
+    );
+}
