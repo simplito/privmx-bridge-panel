a7111ca1f8446248d66a2dedb3cb8eb92e9e8e29
Witold Pazderski <witold.pazderski@gmail.com>
2025-09-29 19:46:34 +0200

    Fix Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable import/no-default-export */
 /* eslint-disable import/no-extraneous-dependencies */
+import * as fs from "fs";
 import * as nodePath from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -23,6 +26,7 @@ export default defineConfig({
         react(),
         nodePolyfills(),
     ],
+    assetsInclude: ["node_modules/privmx-components/**/*.woff2"],
     resolve: {
         alias: {
             "@": nodePath.resolve(__dirname, "./src/"),
@@ -35,5 +39,38 @@ export default defineConfig({
         watch: {
             usePolling: true,
         },
+        fs: {
+            allow: [".", ...getExtraFsAllowList()],
+        },
     },
 });
+
+function getPrivMxComponentsDependencyVersion(): string {
+    const packageJsonPath = "./package.json";
+    if (!fs.existsSync(packageJsonPath)) {
+        throw new Error("package.json not found");
+    }
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf-8"));
+    const dependencies = packageJson.dependencies ?? {};
+    const privMxComponentsVersion = dependencies["privmx-components"] ?? "";
+    if (privMxComponentsVersion === "") {
+        throw new Error("privmx-components dependency not found in package.json");
+    }
+
+    return privMxComponentsVersion as string;
+}
+
+function getExtraFsAllowList(): string[] {
+    const extraFsAllowList = [];
+
+    const privMxComponentsVersion = getPrivMxComponentsDependencyVersion();
+    if (privMxComponentsVersion.startsWith("file:")) {
+        const privMxComponentsPath = privMxComponentsVersion.substring("file:".length);
+        const privMxComponentsDistPath = nodePath.join(privMxComponentsPath, "dist");
+        if (fs.existsSync(privMxComponentsDistPath)) {
+            extraFsAllowList.push(privMxComponentsDistPath);
+        }
+    }
+
+    return extraFsAllowList;
+}
