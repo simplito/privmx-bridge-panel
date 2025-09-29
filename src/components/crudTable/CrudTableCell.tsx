0bc7723ac23686a56e3b9d4bf42d3ebd453a1cd3
Witold Pazderski <witold.pazderski@gmail.com>
2025-09-29 19:51:03 +0200

    Remove CrudTable

--- src/components/crudTable/CrudTableCell.tsx
+++
@@ -1,7 +0,0 @@
-import { Table } from "@mantine/core";
-
-export type CrudTableCellProps = React.PropsWithChildren;
-
-export function CrudTableCell(props: CrudTableCellProps) {
-    return <Table.Td>{props.children}</Table.Td>;
-}
