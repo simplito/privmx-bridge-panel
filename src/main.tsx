2487edb0d08eece8bab324af97bda00eb6602d0c
Witold Pazderski <witold.pazderski@gmail.com>
2025-09-29 19:47:50 +0200

    Remove unused style imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import "@fontsource-variable/noto-sans";
 import React from "react";
 // eslint-disable-next-line @typescript-eslint/naming-convention
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
-import { customCss } from "./components/rootAppLayout/mantineTheme";
 import "./global.scss";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -14,7 +12,3 @@ root.render(
         <RouterProvider router={router} />
     </React.StrictMode>,
 );
-
-const style = document.createElement("style");
-style.innerHTML = customCss;
-document.head.appendChild(style);
